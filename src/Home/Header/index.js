c5f7d9c47d97d00e819067edf7bb9cbd8831b5c2
LainVellio <cvellio@gmail.com>
2018-07-28 20:09:44 +0300

    init commit

---
+++ src/Home/Header/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = styled.header`
+  background: linear-gradient(
+    151.09deg,
+    #00b0de 0.36%,
+    #01aedc 4.04%,
+    #02abdb 7.77%,
+    #02abdb 11.48%,
+    #02abdb 11.7%,
+    #196ebd 100%
+  );
+  height: 801px;
+`;
+
+const Logo = styled.div``;
+
+export default function(props) {
+  return (
+    <div>
+      <Header>
+        <div className="container">zsdf</div>
+      </Header>
+      <div />
+    </div>
+  );
+}
