83e44eabcc6916155f2fdd61c7bdd99baa1ce0e0
LainVellio <cvellio@gmail.com>
2018-07-30 19:07:32 +0300

    Add Title and Search Button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import "./Fonts/index.css";
 
 import Home from "./Home";
 
--- src/Home/Header/index.js
+++ src/Home/Header/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import logo from "./Logo.svg";
+import plane from "./Plane.svg";
 
 const Header = styled.header`
   background: linear-gradient(
@@ -25,10 +26,104 @@ const LogoImg = styled.img``;
 
 const LogoText = styled.div`
   padding-left: 12px;
-  font-family: Arial;
-  line-height: 25px;
+  font-family: Roboto, sans-serif;
   font-size: 20px;
+  line-height: 25px;
   color: #ffffff;
+`;
+
+const Title = styled.div`
+  display: flex;
+  margin-top: 254px;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TitleTop = styled.div`
+  font-family: Roboto, sans-serif;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 48px;
+  color: #ffffff;
+`;
+const TitleBottom = styled.div`
+  font-family: Roboto, sans-serif;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 28px;
+  color: #ffffff;
+`;
+
+const Input = styled.div`
+  flex-direction: row;
+  margin-top: 40px;
+  margin-bottom: 48px;
+`;
+
+const InputDeparture = styled.input`
+  border-radius: 4px 0 0 4px;
+  border: none;
+  size: 224px;
+  padding-top: 18px;
+  padding-bottom: 18px;
+  padding-left: 16px;
+  margin-right: 2px;
+  align-self: baseline;
+`;
+
+const InputArrival = styled.input`
+  border: none;
+  size: 224px;
+  padding-top: 18px;
+  padding-bottom: 18px;
+  padding-left: 16px;
+  margin-right: 2px;
+  align-self: baseline;
+`;
+
+const InputDepartureDate = styled.input`
+  border: none;
+  size: 224px;
+  padding-top: 18px;
+  padding-bottom: 18px;
+  padding-left: 16px;
+  margin-right: 2px;
+  align-self: baseline;
+`;
+
+const InputArrivalDate = styled.input`
+  border: none;
+  padding-top: 18px;
+  padding-bottom: 18px;
+  padding-left: 16px;
+  margin-right: 2px;
+  align-self: baseline;
+`;
+
+const TitleSelect = styled.select`
+  border: none;
+  padding-top: 17px;
+  padding-bottom: 18px;
+  padding-left: 16px;
+  padding-right: 45px;
+  border-radius: 0 4px 4px 0;
+`;
+const Button = styled.button`
+  background: #ff9241;
+  border: none;
+  padding-top: 15px;
+  padding-bottom: 16px;
+  padding-left: 45px;
+  border-radius: 4px;
+  font-family: Roboto, sans-serif;
+  color: white;
+  font-weight: 900;
+  font-size: 28px;
+`;
+
+const ButtonImg = styled.img`
+  margin-left: 24px;
+  margin-right: 24px;
 `;
 
 export default function(props) {
@@ -39,6 +134,23 @@ export default function(props) {
           <LogoImg src={logo} />
           <LogoText>aviasales</LogoText>
         </Logo>
+        <Title>
+          <TitleTop>Поиск дешевых авиабилетов</TitleTop>
+          <TitleBottom>Лучший способ купить авиaбилеты дешево</TitleBottom>
+          <Input>
+            <InputDeparture />
+            <InputArrival />
+            <InputDepartureDate />
+            <InputArrivalDate />
+            <TitleSelect>
+              <option>1 пассажир</option>
+            </TitleSelect>
+          </Input>
+          <Button>
+            Найти билеты
+            <ButtonImg src={plane} />
+          </Button>
+        </Title>
       </div>
     </Header>
   );
